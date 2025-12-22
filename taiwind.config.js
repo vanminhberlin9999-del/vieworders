fc93f1532318a5d793963b6c542faad14f0bbff1
Van Tinh <van-tinh.chu@botiss.com>
2025-12-22 01:20:23 +0100

    add config for container in tailwind

---
+++ taiwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        container: {
+            center: true, // always center
+            padding: '1rem', // default padding
+            screens: {
+                sm: "100%",
+                md: "100%",
+                lg: "960px",
+                xl: "1200px",
+                "2xl": "1760px",
+            },
+        },
+    },
+};
