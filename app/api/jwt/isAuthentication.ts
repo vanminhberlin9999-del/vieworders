786f6af7c4cad6054da7d6b7903f7dde96ccddbb
Van Tinh <van-tinh.chu@botiss.com>
2026-01-01 21:36:44 +0100

    add hashed password and bcryptjs for password
    add fixed hashedpassword in .env
    compare hasedpassword with input password

--- app/api/jwt/isAuthentication.ts
+++ app/api/jwt/isAuthentication.ts
@@ -1,10 +1,18 @@
+import bcrypt from "bcryptjs";
 import process from "node:process";
 
 const next_username = process.env["ADMIN_USERNAME"];
-const next_password = process.env["ADMIN_PASSWORD"]
+const hased_password:string | undefined = process.env["HASHED_PASSWORD"]
 
-function isAuthentication (username:string, password:string){
-    return username === next_username && password === next_password
+async function isAuthentication (username:string, password:string){
+    let isValid = false;
+    if(username != next_username) return false;
+
+    if(username === next_username){
+        if(!hased_password) return false;
+         isValid = await bcrypt.compare(password, hased_password);
+    }
+    return isValid;
 }
 
 export default isAuthentication;
