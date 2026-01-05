132cb76e059461d0e8ee8e2d7878a2726d74a522
Van Tinh <van-tinh.chu@botiss.com>
2026-01-05 08:56:10 +0100

    set variable for project with taiwind

--- taiwind.config.js
+++ taiwind.config.js
@@ -12,5 +12,38 @@ module.exports = {
                 "2xl": "1760px",
             },
         },
+        extend: {
+            fontFamily: {
+                heading: ["var(--font-heading)"],
+                body: ["var(--font-body)", "ui-sans-serif", "system-ui", "sans-serif"],
+
+            },
+            colors: {
+                // topbarBgLight: '#FF2A41', // Hellmodus
+                // topbarBgDark: '#1F2937',  // Dunkelmodus
+            },
+            fontSize: {
+                xs: ["12px", "16px"],
+                sm: ["14px", "20px"],
+                base: ["16px", "24px"],
+                h1: ["48px", {lineHeight: "56px", fontWeight: "700"}],
+                h2: ["36px", "44px"],
+                h3: ["24px", "32px"],
+            },
+            spacing: {
+                18: "18px",
+                22: "22px",
+                56: "56px",
+            },
+            borderRadius: {
+                xl: "20px",
+                '2xl': "28px",
+            },
+            boxShadow: {
+                card: "0 8px 24px rgba(0,0,0,0.08)",
+            },
+        },
     },
+
+
 };
